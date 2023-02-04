c71e22842936e3a192b67157630d3637cf6dcf69
Jorge Christino <jorgecsantofer@gmail.com>
2023-02-04 19:36:29 -0300

    Project created

---
+++ src/main/java/com/project_springboot/springbootmongodb/SpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.project_springboot.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbApplication.class, args);
+	}
+
+}
