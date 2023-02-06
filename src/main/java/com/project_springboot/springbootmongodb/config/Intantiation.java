4afaa030280217b8c03c5479633b3f460910c4af
Jorge Christino <jorgecsantofer@gmail.com>
2023-02-06 17:21:11 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/project_springboot/springbootmongodb/config/Intantiation.java
@@ -0,0 +1,29 @@
+package com.project_springboot.springbootmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.project_springboot.springbootmongodb.domain.User;
+import com.project_springboot.springbootmongodb.repositories.UserRepository;
+
+@Configuration
+public class Intantiation implements CommandLineRunner{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        
+    }
+    
+}
