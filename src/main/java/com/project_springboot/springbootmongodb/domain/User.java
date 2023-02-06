3c9d55b2331b1bc80e4b6e3a3a631606af543053
Jorge Christino <jorgecsantofer@gmail.com>
2023-02-06 16:28:32 -0300

    Entity User e REST

---
+++ src/main/java/com/project_springboot/springbootmongodb/domain/User.java
@@ -0,0 +1,67 @@
+package com.project_springboot.springbootmongodb.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private int id;
+    private String name;
+    private String email;
+
+    public User(){
+
+    }
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+}
